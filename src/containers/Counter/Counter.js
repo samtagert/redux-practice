75f93d805bbdaef41b4fd6ae88e093a7e536354b
Sam Tagert <samtagert@gmail.com>
2018-05-07 15:48:38 -0700

    updating state through redux immutably

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -14,6 +14,13 @@ class Counter extends Component {
                 <CounterControl label="Decrement" clicked={this.props.onDecrementCounter}  />
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter}  />
+                <hr />
+                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <ul>
+                    {this.props.storedResults.map(r => (
+                        <li onClick={this.props.onDeleteResult} key={r.id}>{r.value}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
@@ -21,7 +28,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter
+        ctr: state.counter,
+        storedResults: state.results
     }
 }
 
@@ -31,6 +39,8 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: 'DECREMENT'}),
         onAddCounter: () => dispatch({type: 'ADD', value: 5}),
         onSubstractCounter: () => dispatch({type: 'SUBSTRACT', value: 5}),
+        onStoreResult: () => dispatch({type: 'STORE_RESULT'}),
+        onDeleteResult: () => dispatch({type: 'DELETE_RESULT'})
     }
 }
 
