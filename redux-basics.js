65de003efdb8f9694220fa968fc059c787e3327d
Sam Tagert <samtagert@gmail.com>
2018-05-07 13:32:15 -0700

    basic redux skeleton setup

---
+++ redux-basics.js
@@ -0,0 +1,14 @@
+const redux = require('redux')
+const createStore = redux.createStore
+
+const initialState = {
+  counter: 0
+}
+
+const rootReducer = (state = initialState, action) => {
+  return state
+}
+
+const store = createStore(rootReducer)
+
+console.log(store.getState())
