c134cd1af4358ce1d581c42ed5f7fe475a9733b2
Sam Tagert <samtagert@gmail.com>
2018-05-07 13:39:05 -0700

    dispatcher hook up

--- redux-basics.js
+++ redux-basics.js
@@ -6,9 +6,26 @@ const initialState = {
 }
 
 const rootReducer = (state = initialState, action) => {
+  if (action.type === 'INC_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + 1
+    }
+  }
+  if (action.type === 'ADD_COUNTER') {
+    return {
+      ...state,
+      counter: state.counter + action.value
+    }
+  }
   return state
 }
 
 const store = createStore(rootReducer)
+
+console.log(store.getState())
+
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 10})
 
 console.log(store.getState())
