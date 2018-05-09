cb2c4045eba758e4cb88057414e05199b21f6ae9
Sam Tagert <samtagert@gmail.com>
2018-05-08 23:53:32 -0700

    separate reducer into multiple

--- src/containers/Counter/Counter.js
+++ src/containers/Counter/Counter.js
@@ -16,7 +16,7 @@ class Counter extends Component {
                 <CounterControl label="Add 5" clicked={this.props.onAddCounter}  />
                 <CounterControl label="Subtract 5" clicked={this.props.onSubstractCounter}  />
                 <hr />
-                <button onClick={this.props.onStoreResult}>Store Result</button>
+                <button onClick={() => this.props.onStoreResult(this.props.ctr)}>Store Result</button>
                 <ul>
                     {this.props.storedResults.map(r => (
                         <li onClick={() => this.props.onDeleteResult(r.id)} key={r.id}>{r.value}</li>
@@ -29,8 +29,8 @@ class Counter extends Component {
 
 const mapStateToProps = state => {
     return {
-        ctr: state.counter,
-        storedResults: state.results
+        ctr: state.ctr.counter,
+        storedResults: state.res.results
     }
 }
 
@@ -40,7 +40,7 @@ const mapDispatchToProps = dispatch => {
         onDecrementCounter: () => dispatch({type: actionTypes.DECREMENT}),
         onAddCounter: () => dispatch({type: actionTypes.ADD, value: 5}),
         onSubstractCounter: () => dispatch({type: actionTypes.SUBSTRACT, value: 5}),
-        onStoreResult: () => dispatch({type: actionTypes.STORE_RESULT}),
+        onStoreResult: (result) => dispatch({type: actionTypes.STORE_RESULT, result}),
         onDeleteResult: (id) => dispatch({type: actionTypes.DELETE_RESULT, id})
     }
 }
--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions'
+import * as actionTypes from '../actions'
 
 const initialState = {
-  counter: 0,
-  results: []
+  counter: 0
 }
 
 const reducer = (state = initialState, action) => {
@@ -26,16 +25,6 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,        
         counter: state.counter - action.value        
-      }
-    case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: state.counter})
-      }
-    case actionTypes.DELETE_RESULT:
-      return {
-        ...state,
-        results: state.results.filter(r => r.id !== action.id)
       }
     }
   return state
